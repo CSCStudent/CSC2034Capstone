b32e5fe6914b063e46052a4bac834e676516657b
CarterRL <97972143+CarterRL@users.noreply.github.com>
2023-04-05 13:40:43 -0600

    Create S02507413.cpp
    Created S02507413.cpp

---
+++ S02507413.cpp
@@ -0,0 +1,14 @@
+// Carter Lyle
+// CSC2034
+// 4/05/2023
+
+// implementation file for student S01426313
+// CSC2034 Spring 2023
+// Capstone project
+namespace S02507413 
+{
+	void run()
+  {
+		std::cout << "hello from S02507413" << std::endl;
+	}
+}
