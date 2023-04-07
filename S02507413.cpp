ea3fae2ebfcc5119acf1b4f684051147a12ee752
CarterRL <97972143+CarterRL@users.noreply.github.com>
2023-04-07 16:34:34 -0600

    Update S02507413.cpp

--- S02507413.cpp
+++ S02507413.cpp
@@ -2,13 +2,15 @@
 // CSC2034
 // 4/05/2023
 
+#include "S02507413.h"
+
 // implementation file for student S01426313
 // CSC2034 Spring 2023
 // Capstone project
 namespace S02507413 
 {
 	void run()
-  {
+	{
 		std::cout << "hello from S02507413" << std::endl;
 	}
 }
