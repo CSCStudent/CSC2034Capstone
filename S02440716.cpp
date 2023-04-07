7777401178b8207488b70ffc103f8275ea73ea9f
Aaron <aaron.h.johnson.21@gmail.com>
2023-04-07 16:30:56 -0600

    adding s02440716

---
+++ S02440716.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "S02440716.h"
+
+// implementation file for student S02440716
+// CSC2034 Spring 2023
+// Capstone project
+namespace S02440716 {
+	void run() {
+		std::cout << "hello from S02440716" << std::endl;
+	}
+}
