cbb5cace9c04155449104385cce15d639425a8c0
Josh Circenis <jecircenis@gmail.com>
2023-04-07 16:30:34 -0600

    Added namespace S02800493

---
+++ S02800493.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "S02800493.h"
+
+// Creating namespace for S02800493
+namespace S02800493 {
+    void run() {
+        std::cout << "hello from S02800493" << std::endl; 
+    }
+}
---
+++ S02800493.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace S02800493
+{
+	void run(); 
+}
