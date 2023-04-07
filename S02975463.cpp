559f32bcc96107dd6964857f38e1b55085b9d4f3
Lenovo <htartakovskyi@student.cccs.edu>
2023-04-07 13:01:12 -0600

    S02975463

---
+++ S02975463.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// implementation file for student S01426313
+// CSC2034 Spring 2023
+// Capstone project
+namespace S02975463 {
+	void run() {
+		std::cout << "hello from S02975463" << std::endl;
+	}
+}
+
---
+++ S02975463.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace S02975463
+{
+	void run();
+}
