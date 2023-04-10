c7c7cced19712eb1217fe910f6282368f8652d19
Jose <jose.al.flores04@gmail.com>
2023-04-10 08:42:03 -0600

    S02861187

---
+++ S02861187.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "S02861187.h"
+
+/*
+* CSC2034: C++ Programming
+* Spring 2023 Capstone
+*/
+
+namespace S02861187 {
+	void run() {
+		std::cout << "Hello from S02861187" << std::endl;
+	}
+}
