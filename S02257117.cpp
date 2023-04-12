d339156880b41779a31ce29f766a5cd7ff1a9761
Nathan McIntyre <nmcintyre2@student.cccs.edu>
2023-04-12 12:34:11 -0600

    Should be creating a new branch and my .cpp and . h files - S02257117(Nathan McIntyre)

---
+++ S02257117.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// implementation file for student S02257117
+// CSC2034 Spring 2023
+// Capstone project
+
+namespace S02257117
+{
+	void run()
+	{
+		std::cout << "Hello from S02257117! " << std:: endl;
+	}
+}
